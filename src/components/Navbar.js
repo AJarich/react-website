9512a0e7d7cd3fa4379993fd8052af87b95ada5c
Andrea Jarich <glenstone93@yahoo.com>
2022-04-20 18:38:32 -0500

    Created basic layout for Navbar. Added font-awesome link in index.html.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,45 @@
 import React, { useState } from 'react';
+import {Link} from 'react-router-dom';
 
-function Navbar(){
-  return{
-    <>
+export default function Navbar(){
+
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  return (
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo">
-            TRVL
+            <i class="fa fa-tree" aria-hidden="true"></i>TRVL
           </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? 'fa fa-times' : 'fa fa-bars'}></i>
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/photos" className="nav-links" onClick={closeMobileMenu}>
+                Photos
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/sign-up" className="nav-links-mobile" onClick={closeMobileMenu}>
+                Sign-up
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
-    </>
-  };
+  );
 }
