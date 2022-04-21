8030d298e41fbcfe2162a0971a1be61c56372519
Andrea Jarich <glenstone93@yahoo.com>
2022-04-20 20:23:30 -0500

    Added button. Coded button. Added css for navbar, app, and button.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom';
+import { Button } from './Button';
+import "./Navbar.css";
 
 export default function Navbar(){
 
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+   if (window.innerWidth <= 960) {
+     setButton(false);
+   } else {
+     setButton(true);
+   }
+ };
+
+useEffect(() => {
+  showButton();
+}, []);
+
+  window.addEventListener('resize', showButton);
 
   return (
       <nav className="navbar">
@@ -33,12 +50,17 @@ export default function Navbar(){
                 Photos
               </Link>
             </li>
-            <li className="nav-item">
-              <Link to="/sign-up" className="nav-links-mobile" onClick={closeMobileMenu}>
-                Sign-up
-              </Link>
+            <li>
+              <Link
+               to='/sign-up'
+               className='nav-links-mobile'
+               onClick={closeMobileMenu}
+             >
+               Sign Up
+             </Link>
             </li>
           </ul>
+          {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
         </div>
       </nav>
   );
